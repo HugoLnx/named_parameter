cd7560d0c9b79e822643eadff7316edc7107c4f4
Hugo Roque <hugolnx@gmail.com>
2012-08-02 01:41:49 -0300

    named parameter now in singleton too

--- lib/named_parameter/named_parameter.rb
+++ lib/named_parameter/named_parameter.rb
@@ -1,10 +1,15 @@
 module NamedParameter
   def named(def_return)
-    method = self.instance_method(@last_method_added)
+    method = @last_method_added
     NamedParameter::NamedMethodTransmuter.transmute method
   end
 
   def method_added(method_name)
-    @last_method_added = method_name
+    @last_method_added = self.instance_method(method_name)
+  end
+
+
+  def singleton_method_added(method_name)
+    @last_method_added = self.method(method_name)
   end
 end
