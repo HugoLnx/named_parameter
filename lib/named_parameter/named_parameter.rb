c0818283fe263e1d84d617c018ea05fbe5f2d3e9
Hugo Roque <hugolnx@gmail.com>
2012-08-07 10:15:55 -0300

    named singleton method in class << self;end

--- lib/named_parameter/named_parameter.rb
+++ lib/named_parameter/named_parameter.rb
@@ -4,6 +4,15 @@ module NamedParameter
     NamedParameter::NamedMethodTransmuter.transmute method
   end
 
+  def self.extended(klass)
+    klass.singleton_class.instance_eval do
+      define_singleton_method :named do |def_return|
+        method = klass.instance_variable_get(:@last_method_added)
+        NamedParameter::NamedMethodTransmuter.transmute method
+      end
+    end
+  end
+
   def method_added(method_name)
     @last_method_added = self.instance_method(method_name)
   end
