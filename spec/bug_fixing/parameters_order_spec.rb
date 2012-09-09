cd15f4c291b296d49306e075385df0082bb98aeb
Hugo Roque <hugolnx@gmail.com>
2012-09-08 22:43:45 -0300

    refactoring tests of bugfix to be more expressive

--- spec/bug_fixing/parameters_order_spec.rb
+++ spec/bug_fixing/parameters_order_spec.rb
@@ -1,275 +1,251 @@
 require 'spec_helper'
 
-describe "BugFix: Parameters order spec" do
+describe "BugFix: should suport any kind use combination of optional parameters" do
   describe 'Named Parameter' do
     context %q{named def whatever(a="z",b="z",c="z",d="z",e="z",f="z")} do
-      context "inline" do
-        specify "parameters maintain order" do
-          class SomeClass
-            extend NamedParameter
+      specify "inline" do
+        class SomeClass
+          extend NamedParameter
 
-            named def whatever(a="z",b="z",c="z",d="z",e="z",f="z")
-              return [a,b,c,d,e,f]
-            end
+          named def whatever(a="z",b="z",c="z",d="z",e="z",f="z")
+            return [a,b,c,d,e,f]
           end
-
-          obj = SomeClass.new
-          
-          obj.whatever(
-            a: "a",
-            c: "c",
-            d: "d",
-            f: "f"
-          ).should == %w{a z c d z f}
         end
+
+        obj = SomeClass.new
+        
+        obj.whatever(
+          a: "a",
+          c: "c",
+          d: "d",
+          f: "f"
+        ).should == %w{a z c d z f}
       end
 
-      context "above" do
-        specify "parameters maintain order" do
-          class SomeClass
-            extend NamedParameter
+      specify "above" do
+        class SomeClass
+          extend NamedParameter
 
-            named
-            def whatever(a="z",b="z",c="z",d="z",e="z",f="z")
-              return [a,b,c,d,e,f]
-            end
+          named
+          def whatever(a="z",b="z",c="z",d="z",e="z",f="z")
+            return [a,b,c,d,e,f]
           end
-
-          obj = SomeClass.new
-          
-          obj.whatever(
-            a: "a",
-            c: "c",
-            d: "d",
-            f: "f"
-          ).should == %w{a z c d z f}
         end
+
+        obj = SomeClass.new
+        
+        obj.whatever(
+          a: "a",
+          c: "c",
+          d: "d",
+          f: "f"
+        ).should == %w{a z c d z f}
       end
     end
 
     context %q{named def whatever(a="z",b="z",c="z", \n d="z",e="z",f="z")} do
-      context "inline" do
-        specify "parameters maintain order" do
-          class SomeClass
-            extend NamedParameter
+      specify "inline" do
+        class SomeClass
+          extend NamedParameter
 
-            named def whatever(a="z",b="z",c="z",
-                               d="z",e="z",f="z")
-              return [a,b,c,d,e,f]
-            end
+          named def whatever(a="z",b="z",c="z",
+                             d="z",e="z",f="z")
+            return [a,b,c,d,e,f]
           end
-
-          obj = SomeClass.new
-          
-          obj.whatever(
-            a: "a",
-            c: "c",
-            d: "d",
-            f: "f"
-          ).should == %w{a z c d z f}
         end
+
+        obj = SomeClass.new
+        
+        obj.whatever(
+          a: "a",
+          c: "c",
+          d: "d",
+          f: "f"
+        ).should == %w{a z c d z f}
       end
 
-      context "above" do
-        specify "parameters maintain order" do
-          class SomeClass
-            extend NamedParameter
+      specify "above" do
+        class SomeClass
+          extend NamedParameter
 
-            named
-            def whatever(a="z",b="z",c="z",
-                         d="z",e="z",f="z")
-              return [a,b,c,d,e,f]
-            end
+          named
+          def whatever(a="z",b="z",c="z",
+                       d="z",e="z",f="z")
+            return [a,b,c,d,e,f]
           end
-
-          obj = SomeClass.new
-          
-          obj.whatever(
-            a: "a",
-            c: "c",
-            d: "d",
-            f: "f"
-          ).should == %w{a z c d z f}
         end
+
+        obj = SomeClass.new
+        
+        obj.whatever(
+          a: "a",
+          c: "c",
+          d: "d",
+          f: "f"
+        ).should == %w{a z c d z f}
       end
     end
 
 
     context %q{named def whatever(a="z",b="z",c="z", d="z",e,f)} do
-      context "inline" do
-        specify "parameters maintain order" do
-          class SomeClass
-            extend NamedParameter
+      specify "inline" do
+        class SomeClass
+          extend NamedParameter
 
-            named def whatever(a,b,c="z", d="z",e="z",f="z")
-              return [a,b,c,d,e,f]
-            end
+          named def whatever(a,b,c="z", d="z",e="z",f="z")
+            return [a,b,c,d,e,f]
           end
-
-          obj = SomeClass.new
-          
-          obj.whatever(
-            a: "a",
-            b: "b",
-            c: "c",
-            d: "d",
-            f: "f"
-          ).should == %w{a b c d z f}
         end
+
+        obj = SomeClass.new
+        
+        obj.whatever(
+          a: "a",
+          b: "b",
+          c: "c",
+          d: "d",
+          f: "f"
+        ).should == %w{a b c d z f}
       end
 
-      context "above" do
-        specify "parameters maintain order" do
-          class SomeClass
-            extend NamedParameter
+      specify "above" do
+        class SomeClass
+          extend NamedParameter
 
-            named
-            def whatever(a,b,c="z", d="z",e="z",f="z")
-              return [a,b,c,d,e,f]
-            end
+          named
+          def whatever(a,b,c="z", d="z",e="z",f="z")
+            return [a,b,c,d,e,f]
           end
-
-          obj = SomeClass.new
-          
-          obj.whatever(
-            a: "a",
-            b: "b",
-            c: "c",
-            d: "d",
-            f: "f"
-          ).should == %w{a b c d z f}
         end
+
+        obj = SomeClass.new
+        
+        obj.whatever(
+          a: "a",
+          b: "b",
+          c: "c",
+          d: "d",
+          f: "f"
+        ).should == %w{a b c d z f}
       end
     end
 
-    context %q{named def whatever(a,b="z",c=lambda{|arg1,arg2| arg1 + "test"})} do
-      context "inline" do
-        specify "parameters maintain order" do
-          class SomeClass
-            extend NamedParameter
+    context %q{named def whatever(a,b="z",c=lambda{|arg1,arg2| arg1[1,2] + "test"})} do
+      specify "inline" do
+        class SomeClass
+          extend NamedParameter
 
-            named def whatever(a,b="z",c=lambda{|arg1,arg2| arg1 + "test"},d="z")
-              return [a,b,c,d]
-            end
+          named def whatever(a,b="z",c=lambda{|arg1,arg2| arg1[1,2] + "test"},d="z")
+            return [a,b,c,d]
           end
-
-          obj = SomeClass.new
-          
-          return_ = obj.whatever(
-            a: "a",
-            d: "d"
-          )
-          return_[0].should == "a"
-          return_[1].should == "z"
-          return_[2].should be_a Proc
-          return_[3].should == "d"
         end
+
+        obj = SomeClass.new
+        
+        return_ = obj.whatever(
+          a: "a",
+          d: "d"
+        )
+        return_[0].should == "a"
+        return_[1].should == "z"
+        return_[2].should be_a Proc
+        return_[3].should == "d"
       end
 
-      context "above" do
-        specify "parameters maintain order" do
-          class SomeClass
-            extend NamedParameter
+      specify "above" do
+        class SomeClass
+          extend NamedParameter
 
-            named
-            def whatever(a,b="z",c=lambda{|arg1,arg2| arg1 + "test"},d="z")
-              return [a,b,c,d]
-            end
+          named
+          def whatever(a,b="z",c=lambda{|arg1,arg2| arg1[1,2] + "test"},d="z")
+            return [a,b,c,d]
           end
-
-          obj = SomeClass.new
-          
-          return_ = obj.whatever(
-            a: "a",
-            d: "d"
-          )
-          return_[0].should == "a"
-          return_[1].should == "z"
-          return_[2].should be_a Proc
-          return_[3].should == "d"
         end
+
+        obj = SomeClass.new
+        
+        return_ = obj.whatever(
+          a: "a",
+          d: "d"
+        )
+        return_[0].should == "a"
+        return_[1].should == "z"
+        return_[2].should be_a Proc
+        return_[3].should == "d"
       end
     end
 
     context %q{named def whatever(a,b="z",c="z,z",d="z")} do
-      context "inline" do
-        specify "parameters maintain order" do
-          class SomeClass
-            extend NamedParameter
+      specify "inline" do
+        class SomeClass
+          extend NamedParameter
 
-            named def whatever(a,b="z",c="z,z",d="z")
-              return [a,b,c,d]
-            end
+          named def whatever(a,b="z",c="z,z",d="z")
+            return [a,b,c,d]
           end
-
-          obj = SomeClass.new
-          
-          obj.whatever(
-            a: "a",
-            d: "d"
-          ).should == %w{a z z,z d}
         end
+
+        obj = SomeClass.new
+        
+        obj.whatever(
+          a: "a",
+          d: "d"
+        ).should == %w{a z z,z d}
       end
 
-      context "above" do
-        specify "parameters maintain order" do
-          class SomeClass
-            extend NamedParameter
+      specify "above" do
+        class SomeClass
+          extend NamedParameter
 
-            named
-            def whatever(a,b="z",c="z,z",d="z")
-              return [a,b,c,d]
-            end
+          named
+          def whatever(a,b="z",c="z,z",d="z")
+            return [a,b,c,d]
           end
-
-          obj = SomeClass.new
-          
-          obj.whatever(
-            a: "a",
-            d: "d"
-          ).should == %w{a z z,z d}
         end
+
+        obj = SomeClass.new
+        
+        obj.whatever(
+          a: "a",
+          d: "d"
+        ).should == %w{a z z,z d}
       end
     end
 
     context %q{named def whatever(a,b="z",c=/z,z/,d="z")} do
-      context "inline" do
-        specify "parameters maintain order" do
-          class SomeClass
-            extend NamedParameter
+      specify "inline" do
+        class SomeClass
+          extend NamedParameter
 
-            named def whatever(a,b="z",c=/z,z/,d="z")
-              return [a,b,c,d]
-            end
+          named def whatever(a,b="z",c=/z,z/,d="z")
+            return [a,b,c,d]
           end
-
-          obj = SomeClass.new
-          
-          obj.whatever(
-            a: "a",
-            d: "d"
-          ).should == ["a", "z", /z,z/, "d"]
         end
+
+        obj = SomeClass.new
+        
+        obj.whatever(
+          a: "a",
+          d: "d"
+        ).should == ["a", "z", /z,z/, "d"]
       end
 
-      context "above" do
-        specify "parameters maintain order" do
-          class SomeClass
-            extend NamedParameter
+      specify "above" do
+        class SomeClass
+          extend NamedParameter
 
-            named
-            def whatever(a,b="z",c=/z,z/,d="z")
-              return [a,b,c,d]
-            end
+          named
+          def whatever(a,b="z",c=/z,z/,d="z")
+            return [a,b,c,d]
           end
-
-          obj = SomeClass.new
-          
-          obj.whatever(
-            a: "a",
-            d: "d"
-          ).should == ["a", "z", /z,z/, "d"]
         end
+
+        obj = SomeClass.new
+        
+        obj.whatever(
+          a: "a",
+          d: "d"
+        ).should == ["a", "z", /z,z/, "d"]
       end
     end
 
